6c0505917df40d8c3af89e8554447af9e2556b85
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2019-12-14 20:54:32 -0500

    Updates

---
+++ precache-manifest.c1b07d840ab9ff869c33912a85bcdd8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6606605779704966937d648cf0a8838f",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "c18e64ffc43b8d13a3c6",
+    "url": "/todoapp/static/css/main.619dc0c8.chunk.css"
+  },
+  {
+    "revision": "ddc415ca4d8fc0a06dc2",
+    "url": "/todoapp/static/js/2.c1afb69f.chunk.js"
+  },
+  {
+    "revision": "c18e64ffc43b8d13a3c6",
+    "url": "/todoapp/static/js/main.357f4c83.chunk.js"
+  },
+  {
+    "revision": "80f8f6c5b874c7fc3aeb",
+    "url": "/todoapp/static/js/runtime-main.e9ca35db.js"
+  }
+]);
